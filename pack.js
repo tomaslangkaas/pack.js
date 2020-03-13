9838837127688d62498e94fdeee81f024908becf
tomaslangkaas <tomaslangkaas@users.noreply.github.com>
2020-03-14 00:57:12 +0100

    Create pack.js

---
+++ pack.js
@@ -0,0 +1,33 @@
+function pack(inDigits, inBases, outBases) {
+  var digits = inDigits.slice(0),
+    digitsLeft = digits.length,
+    result = [],
+    inPos = 0,
+    outPos = 0,
+    remainder,
+    inBase,
+    outBase,
+    maxBase = 94906265;
+  while (digitsLeft) {
+    remainder = 0;
+    outBase = outBases[outPos] || outBase;
+    if (outBase > maxBase) {
+      outBase = maxBase;
+    } 
+    inBase = inBases[inBases.length - 1];
+    for (inPos = digitsLeft - 1; inPos >= 0; inPos--) {
+      inBase = inBases[inPos] || inBase;
+      if (inBase > maxBase) {
+        inBase = maxBase; // handle infinite bases
+      }      
+      remainder = digits[inPos] + remainder * inBase;
+      digits[inPos] = Math.floor(remainder / outBase);
+      remainder -= digits[inPos] * outBase;
+      if (digitsLeft == inPos + 1 && digits[inPos] == 0) {
+        digitsLeft--;
+      }
+    }
+    result[outPos++] = remainder;
+  }
+  return result;
+}
